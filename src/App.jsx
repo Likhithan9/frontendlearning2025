25615969d076bf20c18afab53b6a04efc0992eee
Likhithan9 <likhitha9102006@gmail.com>
2025-10-16 10:32:27 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+const App=()=>{
+    return(
+      <>
+      <Header/>{" "}
+        <div>
+          <h1>Hello from React</h1>
+          <p>I am jahnavi , from AIML</p>
+          </div>
+          </>
+    );
+}
+export default App;
